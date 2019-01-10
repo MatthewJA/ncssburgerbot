296d4e4ca2aac14221b38dffbe8b5e2d049c8988
MatthewJA <matthew.alger@gmail.com>
2019-01-11 09:35:23 +1100

    added orders

--- order.py
+++ order.py
@@ -1,12 +1,17 @@
 from flaskapp import app
 from flask import request
 
+past_orders = []
+
 @app.route('/order', methods=['GET', 'POST'])
 def order():
     order = request.values.get('text')
     user = request.values.get('user_name')
+    past_orders.append((order, user))
     return f'ok {user}, I ordered you a {order}.'
 
 @app.route('/orders', methods=['GET', 'POST'])
 def orders():
-    pass
+    list_of_orders = [f'{order} for {user}' for order, user in past_orders]
+    return ', '.join(list_of_orders)
+

d1af573e2d1145b0c80c6b2fa9daf5ae79bc9668
MatthewJA <matthew.alger@gmail.com>
2019-01-11 09:27:56 +1100

    added basic bot

---
+++ order.py
@@ -0,0 +1,12 @@
+from flaskapp import app
+from flask import request
+
+@app.route('/order', methods=['GET', 'POST'])
+def order():
+    order = request.values.get('text')
+    user = request.values.get('user_name')
+    return f'ok {user}, I ordered you a {order}.'
+
+@app.route('/orders', methods=['GET', 'POST'])
+def orders():
+    pass
---
+++ server.py
@@ -0,0 +1,6 @@
+from flaskapp import app
+import order
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

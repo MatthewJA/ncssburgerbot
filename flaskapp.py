d101c2b8d39e824644209737c5189e581a335b8d
MatthewJA <matthew.alger@gmail.com>
2019-01-11 10:08:34 +1100

    added mongodb

--- flaskapp.py
+++ flaskapp.py
@@ -1,3 +1,9 @@
 from flask import Flask
+import pymongo
+import os
+
+mongodb_uri = os.environ.get('MONGODB_URI')
+client = pymongo.MongoClient(mongodb_uri)
+db = client.get_default_database()
 
 app = Flask(__name__)
--- order.py
+++ order.py
@@ -1,20 +1,20 @@
-from flaskapp import app
+from flaskapp import app, db
 from flask import request
-
-past_orders = []
 
 @app.route('/order', methods=['GET', 'POST'])
 def order():
     order = request.values.get('text')
     user = request.values.get('user_name')
-    past_orders.append((order, user))
+    db.past_orders.insert({'order': order, 'user': user})
     return f'ok {user}, I ordered you a {order}.'
 
 @app.route('/orders', methods=['GET', 'POST'])
 def orders():
-    if len(past_orders) == 0:
+    past_orders = db.past_orders.find()
+    if past_orders.count() == 0:
         return 'no past orders!'
 
-    list_of_orders = [f'{order} for {user}' for order, user in past_orders]
+    list_of_orders = [
+        f'{order["order"]} for {order["user"]}'
+        for order in past_orders]
     return ', '.join(list_of_orders)
-
